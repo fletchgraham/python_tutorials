9e9b7013a7d3f8d17d6f904cd206bab4af25e570
fletchgraham <fletcher@mayostudios.com>
2017-11-14 09:37:41 -0500

    solved print primes problem

--- primes_list.py
+++ primes_list.py
@@ -6,14 +6,19 @@ def list_primes(limit):
     limit = int(limit)
     primes = []
     # append all of the primes to a list
-    for i in range(limit):
+    for i in range(1, limit):
+        is_prime = False
         for j in range(1, i):
-            print(j)
-            if int(i % j) == 0:
+            if int(i % j) == 0 and i != j and j != 1:
+                is_prime = False
                 break
             else:
-                primes.append(i)
+                is_prime = True
+        if is_prime:
+            primes.append(i)
     # return the list
     return primes
 
 print(list_primes(my_limit))
+
+# surely there is a simpler way to make a list of primes.
