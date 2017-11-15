29be70d32f9d5fc30c8eeeeeffe930d83526eadb
fletchgraham <sfletchgraham@gmail.com>
2017-11-15 07:51:47 -0500

    sketched out a multiplication table generator

---
+++ mult_table.py
@@ -0,0 +1,12 @@
+# multiplication table generator
+# i is equal to 1
+# initiate table
+# while i is less than 10
+    # j is equal to 1
+    # initiate row
+    # while j is less than 10
+        # append j x i to row
+        # increment j by 1
+    # append row to table
+    # increment i
+# print table
